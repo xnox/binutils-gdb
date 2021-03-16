5c709728635dd3d9255f6fe655d66b2df4dd91bd
Dimitri John Ledkov <xnox@ubuntu.com>
2021-03-16 00:29:24 +0000

    Add TARGET_KEEP_UNUSED_SECTION_SYMBOLS

--- bfd/coff-aa64.c
+++ bfd/coff-aa64.c
@@ -622,7 +622,7 @@ const bfd_target
   '/',				/* Ar_pad_char.  */
   15,				/* Ar_max_namelen.  */
   0,				/* match priority.  */
-
+  TARGET_KEEP_UNUSED_SECTION_SYMBOLS, /* keep unused section symbols.  */
   bfd_getl64, bfd_getl_signed_64, bfd_putl64,
      bfd_getl32, bfd_getl_signed_32, bfd_putl32,
      bfd_getl16, bfd_getl_signed_16, bfd_putl16, /* Data.  */
